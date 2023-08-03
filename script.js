3e9f49e1262283d3812b8ba0f9a9a603c4085c59
Himanshu Mani Tripathi <Himanshumanitripathi39@gmail.com>
2023-08-03 20:30:10 +0530

    solved the problem of show more button

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
- const accessKey = "XHhJYPugJu7XMoO5tpErEzfNf11MT7Yj7vQ_xxlzZIQ"
- const formEl = document.querySelector("form");
- const inputEl = document.getElementById("search-input");
- const searchResults = document.querySelector(".search-results");
- const showMore = document.getElementById("show-more-button ");
+const accessKey = "XHhJYPugJu7XMoO5tpErEzfNf11MT7Yj7vQ_xxlzZIQ";
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
 
- let inputData = "";
- let page = 1;
+let inputData = "";
+let page = 1;
 
- async function searchImages(){
-    inputData = inputEl.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+async function searchImages() {
+  inputData = inputEl.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
-    const response = await fetch(url);
-    const data = await response.json();
+  const response = await fetch(url);
+  const data = await response.json();
 
-    const results = data.results;
-
-    if(page === 1){
-        searchResults.innerHTML = "";
-    }
-    results.map((result) =>
-{
-     const imageWrapper = document.createElement("div");
-     imageWrapper.classList.add("search-result");
-     const image = document.createElement("img");
-     image.src = result.urls.small;
+  const results = data.results;
+  if (page === 1) {
+    searchResults.innerHTML = "";
+  }
+  results.map((result) => {
+    const imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("search-result");
+    const image = document.createElement("img");
+    image.src = result.urls.small;
     image.alt = result.alt_description;
     const imageLink = document.createElement("a");
     imageLink.href = result.links.html;
@@ -34,20 +32,20 @@
     imageWrapper.appendChild(image);
     imageWrapper.appendChild(imageLink);
     searchResults.appendChild(imageWrapper);
-} );
+  });
 
- page++;
- if(page > 1){
+  page++;
+  if (page > 1) {
     showMore.style.display = "block";
- } 
+  }
 }
 
-formEl.addEventListener("submit", (event) => {
-    event.preventDefault();
-    page = 1;
-    searchImages();
+formEl.addEventListener("submit", () => {
+  event.preventDefault();
+  page = 1;
+  searchImages();
 });
 
 showMore.addEventListener("click", () => {
-    searchImages();
+  searchImages();
 });
